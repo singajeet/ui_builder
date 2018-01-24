2cb2823ceefe8e417df5da3a6f3a4d49d92fb7cf
singajeet <singajeet@gmail.com>
2018-01-24 22:22:45 +0530

    pkg manager changes

--- core/packages.py
+++ core/packages.py
@@ -18,9 +18,43 @@ class PackageManager(object):
         self._config = ConfigParser.ConfigParser()
         self._config.read(os.path.expanduser('~/.uo_builder'))
         self._pkg_install_location = self._config.get(PACKAGE_MANAGER, PKG_INSTALL_LOC)
-        self._archive_mgr = ArchiveManager()
+        self._archive_manager = ArchiveManager()
+
+    def pkg_install_location():
+        doc = "The pkg_install_location property."
+        def fget(self):
+            return self._pkg_install_location
+        def fset(self, value):
+            self._pkg_install_location = value
+        def fdel(self):
+            del self._pkg_install_location
+        return locals()
+    pkg_install_location = property(**pkg_install_location())
+
+    def archive_manager():
+        doc = "The archive_manager property."
+        def fget(self):
+            return self._archive_manager
+        def fset(self, value):
+            self._archive_manager = value
+        def fdel(self):
+            del self._archive_manager
+        return locals()
+    archive_manager = property(**archive_manager())
+
+    def archive_files():
+        doc = "The archive_files property."
+        def fget(self):
+            return self._archive_files
+        def fset(self, value):
+            self._archive_files = value
+        def fdel(self):
+            del self._archive_files
+        return locals()
+    archive_files = property(**archive_files())
 
     def get_archive_files_list(self):
+        self.archive_files = self.archive_manager.load_archives()
 
 
 class ArchiveManager(object):
@@ -75,10 +109,14 @@ class ArchiveManager(object):
         """
         self.archive_file_list = []
         for file in os.listdir(self.archive_drop_location):
-            if os.path.isfile(file):
+            file_path = os.path.join(self.archive_drop_location, file)
+            if os.path.isfile(file_path):
                 try:
-                    util.validate_file(file)
-                    self.archive_file_list.append(file)
+                    util.validate_file(file_path)
+                    if zipfile.is_zipfile(file_path):
+                        self.archive_file_list.append(file_path)
+                    else:
+                        print('File is not zipped and is skipped...{0}'.format(file))
                 except:
                     print('Invalid file and is skipped...{0}'.format(file))
             else:
