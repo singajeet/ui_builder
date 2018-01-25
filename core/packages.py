e05e92db43fa9ec7ea437a93c7a7b2de7236de61
singajeet <singajeet@gmail.com>
2018-01-25 19:12:28 +0530

    Packages, Comps and Templates implemented

--- core/packages.py
+++ core/packages.py
@@ -2,11 +2,195 @@ import ConfigParser
 import uuid
 import os
 import zipfile
+from jinja2 import BaseLoader,TemplateNotFound,Template,Environment
 
 
 PACKAGE_MANAGER='PackageManager'
 PKG_DROP_IN_LOC='package_drop_in_loc'
 PKG_INSTALL_LOC='package_install_loc'
+
+class ComponentLoader(BaseLoader):
+
+    """Docstring for ComponentLoader. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        BaseLoader.__init__(self, path)
+        self.path = path
+
+    def get_source(self, environment, template):
+        """TODO: Docstring for get_source.
+
+        :environment: TODO
+        :returns: TODO
+
+        """
+        path = os.path.join('./', template)
+        if os.path.exists(path):
+            mtime = os.path.getmtime(path)
+            with file(path) as f:
+                source = f.read().decode('utf-8')
+                return source, path, lambda: mtime == os.path.getmtime(path)
+
+class Component(object):
+
+    """Docstring for Component. """
+
+    def __init__(self, name):
+        """TODO: to be defined1. """
+        self.id = uuid.uuid4()
+        self.name = None
+        self.description = None
+        self.type = None
+        self.author = None
+        self.version = None
+        self.template_path = '{0}.comp'.format(name)
+        self.security_id = None
+
+    def load_component(self):
+        """TODO: Docstring for load_component.
+        :returns: TODO
+
+        """
+        self.config_file = ConfigParser.ConfigParser()
+        self.config_file.read('{0}.comp'.format(self.name))
+        if self.name != self.config_file.get('Details', 'Name'):
+            raise NameError('Can not load component as filename[{0}] and name in config does not match'.format(self.name))
+
+        self.description = self.config_file.get('Details', 'Description')
+        self.type = self.config_file.get('Details', 'Type')
+        self.author = self.config_file.get('Details', 'Author')
+        self.version = self.config_file.get('Details', 'Version')
+        self.template_env = Environment(loader = ComponentLoader(self.template_path))
+
+
+class Package(object):
+
+    """Docstring for Package. """
+
+    def __init__(self, location=None):
+        """TODO: to be defined1. """
+        self.id = uuid.uuid4()
+        self.location = location
+        self.config_file = None
+        self.name = None
+        self.description = None
+        self.type = None
+        self.author = None
+        self.url = None
+        self.company = None
+        self.version = None
+        self._comp_name_list = []
+        self._comp_name_id_map = {}
+        self._components = {}
+
+    def config_file():
+        doc = "The config_file property."
+        def fget(self):
+            return self._config_file
+        def fset(self, value):
+            self._config_file = value
+        def fdel(self):
+            del self._config_file
+        return locals()
+    config_file = property(**config_file())
+
+    def location():
+        doc = "The location property."
+        def fget(self):
+            return self._location
+        def fset(self, value):
+            self._location = value
+        def fdel(self):
+            del self._location
+        return locals()
+    location = property(**location())
+
+    def load_details(self):
+        """TODO: Docstring for load_details.
+        :returns: TODO
+
+        """
+        temp_pkg_files = os.path.listdir(self.location)
+        conf_file = ''
+        for f in temp_pkg_files:
+            if d.endswith('.pkg'):
+                conf_file = f
+                break
+
+        if conf_file == '':
+            raise NameError('No config file found for this package')
+        else:
+            self.config_file = ConfigParser.ConfigParser()
+            self.config_file.read(os.path.join(self.location, conf_file))
+            self.name = self.config_file.get('Details', 'Name')
+            self.description = self.config_file.get('Details', 'Description')
+            self.type = self.config_file.get('Details', 'Type')
+            self.author = self.config_file.get('Details', 'Author')
+            self.url = self.config_file.get('Details', 'Url')
+            self.version = self.config_file.get('Details', 'Version')
+            self.company = self.config_file.get('Details', 'Company')
+            self._comp_name_list = self.config_file.get('Details', 'Components').split(',')
+
+    def get_comp_name_list(self):
+        """TODO: Docstring for get_comp_name_list.
+        :returns: TODO
+
+        """
+        if self._comp_name_list is not None:
+            return self._comp_name_list
+        else:
+            self.load_details()
+            return self._comp_name_list
+
+    def load_components(self):
+        """TODO: Docstring for load_components.
+        :returns: TODO
+
+        """
+        if self._comp_name_list is None:
+            self.load_details()
+
+        if self._comp_name_list is not None:
+            for comp_name in self._comp_name_list:
+                comp = Component(comp_name.strip())
+                comp.load_component()
+                self._components[comp_name] = comp
+                self._comp_name_id_map[comp.id] = comp
+        else:
+            raise Exception('Can not load components of package...{0}'.format(self.name))
+
+    def get_components(self):
+        """TODO: Docstring for get_components.
+        :returns: TODO
+
+        """
+        if self._components is not None:
+            return self._components
+        else:
+            self.load_components()
+            return self._components
+
+        return None
+
+    def get_component(self, comp_name):
+        """TODO: Docstring for get_component.
+        :returns: TODO
+
+        """
+        if self._comp_name_list is not None:
+            return self._components[comp_name]
+
+        return None
+
+    def get_component_by_id(self, comp_id):
+        if self._components is not None and self._comp_name_id_map is not None:
+            if self._comp_name_id_map[comp_id] is not None:
+                return self._comp_name_id_map[comp_id]
+            else:
+                return None
+        else:
+            return None
 
 class PackageManager(object):
 
@@ -16,9 +200,9 @@ class PackageManager(object):
         """TODO: to be defined1. """
         self.id = uuid.uuid4()
         self._config = ConfigParser.ConfigParser()
-        self._config.read(os.path.expanduser('~/.uo_builder'))
-        self._pkg_install_location = self._config.get(PACKAGE_MANAGER, PKG_INSTALL_LOC)
-        self._archive_manager = ArchiveManager()
+        self._config.read(os.path.expanduser('~/.ui_builder'))
+        self.pkg_install_location = self._config.get(PACKAGE_MANAGER, PKG_INSTALL_LOC)
+        self.archive_manager = ArchiveManager()
 
     def pkg_install_location():
         doc = "The pkg_install_location property."
@@ -53,8 +237,83 @@ class PackageManager(object):
         return locals()
     archive_files = property(**archive_files())
 
-    def get_archive_files_list(self):
+    def _get_archive_files_list(self):
         self.archive_files = self.archive_manager.load_archives()
+
+    def load_packages(self):
+        """TODO: Docstring for load_packages.
+        :returns: TODO
+
+        """
+        pass
+
+    def activate_packages(self):
+        """TODO: Docstring for activate_packages.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def activate_package(self, package_id):
+        """TODO: Docstring for activate_package.
+        :returns: TODO
+
+        """
+        pass
+
+    def deactivate_package(self, package_id):
+        """TODO: Docstring for deactivate_package.
+
+        :package_id: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def install_packages(self):
+        """TODO: Docstring for install_packages.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def install_package(self, package_id):
+        """TODO: Docstring for install_package.
+
+        :package_id: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def _extract_package(self, file):
+        """TODO: Docstring for extract_package.
+
+        :file: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def _validate_package(self, arg1):
+        """TODO: Docstring for validate_package.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def _register_package(self, package_location):
+        """TODO: Docstring for install_package.
+        :returns: TODO
+
+        """
+        pass
 
 
 class ArchiveManager(object):
@@ -66,19 +325,8 @@ class ArchiveManager(object):
         self.id = uuid.uuid4()
         self._config = ConfigParser.ConfigParser()
         self._config.read(os.path.expanduser('~/.ui_builder.cfg'))
-        self._archive_drop_location = self._config.get(PACKAGE_MANAGER, PKG_DROP_IN_LOC)
-        self._archive_file_list = None
-
-    def config():
-        doc = "The config property."
-        def fget(self):
-            return self._config
-        def fset(self, value):
-            self._config = value
-        def fdel(self):
-            del self._config
-        return locals()
-    config = property(**config())
+        self.archive_drop_location = self._config.get(PACKAGE_MANAGER, PKG_DROP_IN_LOC)
+        self.archive_file_list = None
 
     def archive_drop_location():
         doc = "The archive_drop_location property."
