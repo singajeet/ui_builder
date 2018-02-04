8b717a3236ae9f54b6a1f269165148377f35d24b
singajeet <singajeet@gmail.com>
2018-02-04 22:49:34 +0530

    threading and sync

--- core/provider/id_mgr.py
+++ core/provider/id_mgr.py
@@ -13,14 +13,24 @@ import configparser
 import os
 from ui_builder.core import constants
 
+_id_table = None
 
-def init_tb():
+def _init_tb():
     """TODO: to be defined1. """
     _config = configparser.ConfigParser()
     _config.read(os.path.join(os.path.abspath(constants.CONF_PATH), 'ui_builder.cfg'))
     _db = tinydb.TinyDB(constants.UI_BUILDER_DB_PATH)
-    _id_table = self._db.table('app_ids')
-    return _id_table
+    _id_table = _db.table('app_ids')
+
+def _check_db():
+    """TODO: Docstring for _check_db.
+    :returns: TODO
+
+    """
+    if _id_table is None:
+        _init_tb()
+        if _id_table is None:
+            raise Exception('Can''t find the ID''s table. Kindly check configured metadata database')
 
 def all_id():
     """TODO: Docstring for get.
@@ -29,19 +39,37 @@ def all_id():
     :returns: TODO
 
     """
-    return init_db().all()
+    _check_db()
+    return _id_table.all()
 
 def get_id(obj_name):
     """TODO: Docstring for get_id.
     :returns: TODO
 
     """
-    table = init_db()
-    _id = table.get(tinydb.Query()['obj_name'] == obj_name)
+    _check_db()
+    _id = _id_table.get(tinydb.Query()['obj_name'] == obj_name)
     if _id is not None:
-        return _id
+        return _id['id']
     else:
         _id = uuid.uuid4()
-        table.insert({'obj_name':obj_name, 'id':_id})
+        _id_table.insert({'obj_name':obj_name, 'id':_id})
         return _id
 
+def find_id(obj_name):
+    """TODO: Docstring for find_id.
+    :returns: TODO
+
+    """
+    _check_db()
+    _id = _id_table.get(tinydb.Query()['obj_name'] == obj_name)
+    return _id['id']
+
+def find_name(obj_id):
+    """TODO: Docstring for find_name.
+    :returns: TODO
+
+    """
+    _check_db()
+    _obj_name = _id_table.get(tinydb.Query()['id'] == obj_id)
+    return _obj_name['obj_name']
--- core/provider/tasks.py
+++ core/provider/tasks.py
@@ -7,7 +7,31 @@ Author:
 Date:
     2/4/2018
 """
-from ui_builder.core import constants, id_mgr
+from ui_builder.core import constants
+from ui_builder.core.provider import id_mgr
+from threading import Thread
+import asyncio
+
+
+class HybridTask(Thread):
+
+    """Docstring for HybridTask. """
+
+    def __init__(self, group=None, target=None,name=None, args=(), kwargs={}):
+        """TODO: to be defined1. """
+        super(HybridThread, self).__init__(group=group, target=target, name=name, args=args, kwargs=kwargs)
+        self.loop = asyncio.new_event_loop()
+
+    def start(self):
+        """TODO: Docstring for start.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        super(HybridThread, self).start()
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
 
 
 class ThreadManager(object):
@@ -16,5 +40,7 @@ class ThreadManager(object):
 
     def __init__(self):
         """TODO: to be defined1. """
-        self.id = id_mgr.get_id('ThreadManager')
+        self.name = 'ThreadManager'
+        self.id = id_mgr.get_id(self.name)
+
 
